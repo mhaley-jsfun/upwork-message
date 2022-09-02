dfa68e7643269d96ab702101bace2cdca56cdd65
Ruslan K <ruslankozubovskyi@gmail.com>
2022-09-02 07:00:45 -0300

    s

--- background.js
+++ background.js
@@ -7,12 +7,6 @@ var http = new XMLHttpRequest()
 var url = 'https://upworknotify.herokuapp.com/upwork'
 http.open('POST', url, true);
 //Send the proper header information along with the request
-http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-http.onreadystatechange = function() {//Call a function when the state changes.
-    if(http.readyState == 4 && http.status == 200) {
-        // alert(http.responseText);
-    }
-}
 
 chrome.extension.onConnect.addListener(function (port) {
   port.onMessage.addListener(function (msg) {
