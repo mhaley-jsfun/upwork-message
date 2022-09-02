c022439493f990a71f16693dce218277831380c0
Ruslan K <ruslankozubovskyi@gmail.com>
2022-09-02 05:33:38 -0300

    s

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ http.open('POST', url, true);
 http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 http.onreadystatechange = function() {//Call a function when the state changes.
     if(http.readyState == 4 && http.status == 200) {
-        alert(http.responseText);
+        // alert(http.responseText);
     }
 }
 
